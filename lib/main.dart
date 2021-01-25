73cb206da9154315529a759a136b2b6eae398f36
Jakub <jakub.cekala@sigmaconnectivity.com>
2021-01-25 17:08:01 +0100

    Init

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(NavigationBarApp());
+}
+
+class NavigationBarApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Floating Navigation Bar',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Floating Navigation Bar"),
+      ),
+      body: Container(),
+    );
+  }
+}
